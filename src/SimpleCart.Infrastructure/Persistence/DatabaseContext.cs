0d7e566c90fc953da18a59f94d01c55fdb69d7f7
Manzur Alahi <devmanzur@gmail.com>
2022-01-11 00:43:30 +0600

    added infrastructure with dbcontext"

--- src/SimpleCart.Infrastructure/Persistence/DatabaseContext.cs
+++ src/SimpleCart.Infrastructure/Persistence/DatabaseContext.cs
@@ -1,14 +1,21 @@
 using Microsoft.EntityFrameworkCore;
+using SimpleCart.Core.Interfaces;
 using SimpleCart.Core.Models.CartAggregate;
 using SimpleCart.Core.Models.ProductAggregate;
 using SimpleCart.Core.Models.UserAggregate;
 
-namespace SimpleCart.Core.Interfaces;
+namespace SimpleCart.Infrastructure.Persistence;
 
-public interface IUnitOfWork
+public class DatabaseContext : DbContext, IUnitOfWork
 {
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Cart> Carts { get; set; }
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(DatabaseContext).Assembly);
+    }
 }
